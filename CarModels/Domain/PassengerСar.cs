c73811e6d356edf7ada4c34e74f77d7c0aedc88f
ervaln01 <36621139+ervaln01@users.noreply.github.com>
2022-08-04 10:53:47 +0300

    Update PassengerСar.cs

--- CarModels/Domain/PassengerСar.cs
+++ CarModels/Domain/PassengerСar.cs
@@ -1,42 +1,39 @@
-﻿using PowerLineTest.CarModels.Composition;
+namespace PowerLineTest.CarModels.Domain;
 
-namespace PowerLineTest.CarModels.Domain
+using PowerLineTest.CarModels.Composition;
+
+/// <summary>
+/// Легковой автомобиль. Возможна перевозка пассажиров.
+/// </summary>
+public abstract class PassengerСar : Car
 {
-    /// <summary>
-    /// Легковой автомобиль. Возможна перевозка пассажиров.
-    /// </summary>
-    public abstract class PassengerСar : Car
+    private List<Passenger> passengers = new();
+
+    public int MaximumPassengers { get; protected set; } = 4;
+
+    protected PassengerСar(double tankFuelCapacity, double averageFuelExpend, int accelerationSpeed) 
+        : base(tankFuelCapacity, averageFuelExpend, accelerationSpeed) { }
+
+    protected PassengerСar(double tankFuelCapacity, double averageFuelExpend, int accelerationSpeed, int maximumPassengers) 
+        : base(tankFuelCapacity, averageFuelExpend, accelerationSpeed) => MaximumPassengers = maximumPassengers;
+
+    public override sealed double AverageFuelExpend
     {
-        private List<Passenger> passengers = new();
-
-        public int MaximumPassengers { get; protected set; } = 4;
-
-        protected PassengerСar(double TankFuelCapacity, double AverageFuelExpend, int AccelerationSpeed) 
-            : base(TankFuelCapacity, AverageFuelExpend, AccelerationSpeed) { }
-        
-        protected PassengerСar(double TankFuelCapacity, double AverageFuelExpend, int AccelerationSpeed, int MaximumPassengers) 
-            : base(TankFuelCapacity, AverageFuelExpend, AccelerationSpeed) { this.MaximumPassengers = MaximumPassengers; }
-
-        public override sealed double AverageFuelExpend
-        {
-            get
-            {
-                var tmpAverageFuelExpend = averageFuelExpend;
-
-                foreach (var passenger in passengers) tmpAverageFuelExpend /= 0.94;
-
-                return tmpAverageFuelExpend;
-            }
-            protected set { averageFuelExpend = value; }
-        }
-
-        public double GetDistanceFullTankWithPassengers() => GetDistanceFullTankWithContent();
-        public double GetDistanceCurrentTankWithPassengers() => GetDistanceCurrentTankWithContent();
-
-        public void AddPassenger(Passenger passenger) { if (passengers.Count != MaximumPassengers) passengers.Add(passenger); }
-        public void RemovePassenger(Passenger passenger) => passengers.Remove(passenger);
-        public void RemoveAllPassengers() => passengers.Clear();
-
-        public List<Passenger> GetPassengers() => passengers;
+        get => averageFuelExpend * Math.Pow(0.94, passengers.Count());
+        protected set => averageFuelExpend = value;
     }
+
+    public double GetDistanceFullTankWithPassengers() => GetDistanceFullTankWithContent();
+    public double GetDistanceCurrentTankWithPassengers() => GetDistanceCurrentTankWithContent();
+
+    public void AddPassenger(Passenger passenger) 
+    { 
+        if (passengers.Count != MaximumPassengers) 
+            passengers.Add(passenger); 
+    }
+    
+    public void RemovePassenger(Passenger passenger) => passengers.Remove(passenger);
+    public void RemoveAllPassengers() => passengers.Clear();
+
+    public List<Passenger> GetPassengers() => passengers;
 }
